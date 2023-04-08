c5c6d302e95c68c090294c0ff0cc7ac2dfa63621
sayaoyuri <sayaoyuri.dev@gmail.com>
2023-04-07 23:00:16 -0300

    Play feature finalization

--- js/game.js
+++ js/game.js
@@ -1,61 +1,104 @@
-// const cardQt = parseInt(prompt("Quantidade de cartas?"));
-function checkCardQuantity() {
-  // implement
+const cardList = ['./assets/images/bobrossparrot.gif','./assets/images/explodyparrot.gif', './assets/images/fiestaparrot.gif', './assets/images/metalparrot.gif', './assets/images/revertitparrot.gif', './assets/images/tripletsparrot.gif', './assets/images/unicornparrot.gif'];
+// const cardsDisplay = cardList.concat(cardList);
+// cardsDisplay.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
 }
 
-const cardList = ['./assets/images/bobrossparrot.gif'];
 let round = 0;
 let score = 0;
-let pickedCard = "";
 
-const cards = document.querySelectorAll('.card').forEach((element) => {
-  element.addEventListener('click', play);  
-});
+let cardQt;
 
-function play (element) {
-  console.log(element.currentTarget);
-  element.currentTarget.children[0].classList.add("active");
-  element.currentTarget.children[1].classList.add("active");
-  element.currentTarget.children[1].children[0].setAttribute('data-test', 'face-up-image');
+let quantityCheck;
 
-  pickCard(element.currentTarget);
-  round++;
+do {
+  cardQt = parseInt(prompt("Quantidade de cartas?"));
+
+  if(cardQt % 2 === 0 && cardQt >= 4 && cardQt <= 14) {
+    checkQuantity = false;
+  } else {
+    checkQuantity = true;
+  }
+} while (checkQuantity);
+
+const cardsContainer = document.querySelector('.cards-container');
+cardsContainer.innerHTML = '';
+for (let i = 0; i < cardQt; i++) {
+  cardsContainer.innerHTML += 
+  `<div class="card">
+    <div class="front-face face">
+      <img data-test="face-down-image" src="./assets/images/back.png" alt="">
+    </div>
+    <div class="back-face face">
+      <img src="${cardsDisplay[i]}">
+    </div>
+  </div>`
+  ;
+  
 }
 
-function pickCard (element) {
-  pickedCard === "" ? pickedCard = element.children[1].children[0].getAttribute("src") : checkPick(element);
+let firstPick = "";
+let secondPick = "";
+
+const cards = document.querySelectorAll('.card').forEach((element) => {
+  element.addEventListener('click', pickCard);  
+});
+
+function pickCard (card) {
+    if (firstPick == '') {
+      firstPick = card.currentTarget;
+      
+      card.currentTarget.children[0].classList.add("active");
+      card.currentTarget.children[1].classList.add("active");
+      card.currentTarget.children[1].children[0].setAttribute('data-test', 'face-up-image');
+    } else if (firstPick !== '' && secondPick == '') {
+      secondPick = card.currentTarget;
+      secondPick.children[0].classList.add('active');
+      secondPick.children[1].classList.add('active');
+      secondPick.children[1].children[0].setAttribute('data-test', 'face-up-image');
+
+      checkPicks();
+    }
+
 }
 
 // check if the two selected cards match, if so disables onClick for the matched cards. If not, turns the cards again
-function checkPick (card) {
-  if(card.children[1].children[0].getAttribute("src") === pickedCard) {
-    document.querySelectorAll(`[src="${pickedCard}"`).forEach((card) => {
-      card.parentNode.parentNode.removeEventListener('click', play);
-    });
-
+function checkPicks (card) {
+  if (firstPick.children[1].children[0].getAttribute('src') === secondPick.children[1].children[0].getAttribute('src') ) {
     score++;
-    pickedCard = "";
+    firstPick.removeEventListener('click', pickCard);
+    secondPick.removeEventListener('click', pickCard);
+
+    
+    setTimeout(() => {
+      firstPick = '';
+      secondPick = '';
+    }, 1200);
+    checkWin();
   } else {
-    setTimeout(() => {uncheckCards(card);}, 1000);
+
+    setTimeout(clearPicks, 1000);
   }
+
 }
 
-function uncheckCards (card) {
-  const test = document.querySelectorAll(`[src="${pickedCard}"`);
-  test.forEach((card) => {
-    card.setAttribute('data-test', '');
-    
-    // need to refact
-    console.log(card)
-    card.parentNode.classList.remove('active');
-    card.parentNode.parentNode.children[0].classList.remove('active');
-  });
+function clearPicks () {
+  firstPick.children[0].classList.remove('active');
+  firstPick.children[1].classList.remove('active');
+  firstPick.children[1].children[0].removeAttribute('data-test');
+  firstPick = '';
+  
+  secondPick.children[0].classList.remove('active');
+  secondPick.children[1].classList.remove('active');
+  secondPick.children[1].children[0].removeAttribute('data-test');
+  secondPick = '';
+}
 
-  // works, need to refact
-  card.children[1].children[0].setAttribute('data-test', '');
-
-  card.children[0].classList.remove('active');
-  card.children[1].classList.remove('active');
-
-  pickedCard = "";
+function checkWin () {
+  if(score === cardQt / 2) {
+    alert('VITÒRIA');
+  }
 }
